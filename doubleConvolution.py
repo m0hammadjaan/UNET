48b649ab0c26852e3aba3e9681e332494bcd2f5e
Muhammad Jan <jan4205668@cloud.neduet.edu.pk>
2023-06-04 00:46:15 +0500

    Double Convolution class implemented

---
+++ doubleConvolution.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class doubleConvolution(nn.Module):
+    def __init__(self, inputChannels, outputChannels):
+        super(doubleConvolution, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(inputChannels, outputChannels, 3, 1, 1, bias = False),
+            nn.BatchNorm2d(outputChannels),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(outputChannels, outputChannels, 3, 1, 1, bias = False),
+            nn.BatchNorm2d(outputChannels),
+            nn.ReLU(inplace = True)
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
